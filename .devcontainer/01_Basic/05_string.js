b454c62a8626077c346db542fae667aee942ee27
Tejalbhosale2712 <tejal.bhosale2712@gmail.com>
2025-01-05 09:26:05 +0000

    add the numbers

---
+++ .devcontainer/01_Basic/05_string.js
@@ -0,0 +1,25 @@
+const name="Hitesh"
+const repoCount=50
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('hiteshch')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'));
+
+
+const newstring=gameName.substring(0,4)
+//console.log(newstring);
+ 
+const newstringOne="       Hitesh       "
+//console.log(newstringOne);
+//console.log(newstringOne.trim());
+
+const url="https://hitesh.com/hitesh%20chaoudhari"
+//console.log(url.replace('%20','-'));
+//console.log(url.includes('hitesh'));
+//console.log(url.includes('sundar'));
